d1db51f16b15a9d17785be7b360ff66064a0a8ea
frentzenzero <frentzen.zero@gmail.com>
2018-09-16 21:09:40 +0700

    cek 2

--- objects/user.php
+++ objects/user.php
@@ -1,6 +1,6 @@
 <?php
 class User{
- 
+    //RIO
     // database connection and table name
     private $conn;
     private $table_name = "user";
@@ -73,55 +73,50 @@ class User{
         $stmt->bindParam(":token", $this->token);
       
 
+        
+    
+        // execute query EMAL
         if($stmt->execute()){
-            return true;
+            
+            if($this->mail()){
+                return true;
+            }else{
+                return false;
+            }
         }
     
         return false;
-    
-        // execute query EMAL
-        // if($stmt->execute()){
-            
-        //     if($this->mail()){
-        //         return true;
-        //     }else{
-        //         return false;
-        //     }
-        // }
-    
-        // return false;
         
     }
 
-    // function mail(){
-    //     $email_subject = "Website Contact From:  RentalMobilApps";
-    //         $headers = "From: rentalmobilapps@rentalmbilapps.thekingcorp.org"."\r\n";
-    //         $headers .= "Reply-To:noreply@rentalmbilapps.thekingcorp.org" . "\r\n";
-    //         $headers .= "MIME-Version: 1.0\r\n";
-    //         $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    function mail(){
+        $email_subject = "Website Contact From:  RentalMobilApps";
+            $headers = "From: rentalmobilapps@rentalmbilapps.thekingcorp.org"."\r\n";
+            $headers .= "Reply-To:noreply@rentalmbilapps.thekingcorp.org" . "\r\n";
             
-    //         $message ='<html>';
-    //         $message .='<body>';
-    //             $message .='<div class="mail" style="margin: auto; width: 100%; max-width: 350px; text-align: center; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); border-radius: 30px;">';
-    //                 $message .='<div class="mail-header" style="color: white; background-color: #003365; width: 100%; font-size: 20px; padding: 20px; border-top-left-radius: 25px; border-top-right-radius: 25px;">';
-    //                     $message .='<strong>VERIFIKASI EMAIL DARI <br/>Rental Mobil</strong>';
-    //                 $message .='</div>';
-    //                 $message .='<div class="mail-body" style="color: black; background-color:  #CFE7EA; width: 100%; padding: 20px;">';
-    //                     $message .='<h1>Hai '.$this->nama.', Silahkan lakukan verifikasi email klik tombol ini </h1>';
-    //                     $message .='<a href="https:/tiematweb18.000webhostapp.com/mail/verifikasiEmail.php?token='.$this->token.'"><button style="background-image: linear-gradient(to left, #0025BC , #0071BC); width: 100%; text-align: center; margin: auto; min-height: 40px; color: white; font-size: 30px; cursor: pointer;">Klik disini</button></a>';
-    //                 $message .='</div>';
-    //                 $message .='<div class="mail-footer" style="color: black; background-color: #adadad; width: 100%; font-size: 20px;padding: 20px; border-bottom-left-radius: 25px; border-bottom-right-radius: 25px;">';
-    //                 $message .='</div>';
-    //             $message .='</div>';
-    //         $message .='</body>';
-    //         $message .='</html>';
             
-    //         if(mail($this->email,$email_subject,$message,$headers)){
-    //             return true; 
-    //         }else{
-    //             return false;
-    //         }
-    // }
+            $message ='<html>';
+            $message .='<body>';
+                $message .='<div class="mail" style="margin: auto; width: 100%; max-width: 350px; text-align: center; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); border-radius: 30px;">';
+                    $message .='<div class="mail-header" style="color: white; background-color: #003365; width: 100%; font-size: 20px; padding: 20px; border-top-left-radius: 25px; border-top-right-radius: 25px;">';
+                        $message .='<strong>VERIFIKASI EMAIL DARI <br/>Rental Mobil</strong>';
+                    $message .='</div>';
+                    $message .='<div class="mail-body" style="color: black; background-color:  #CFE7EA; width: 100%; padding: 20px;">';
+                        $message .='<h1>Hai '.$this->nama.', Silahkan lakukan verifikasi email klik tombol ini </h1>';
+                        $message .='<a href="https:/tiematweb18.000webhostapp.com/mail/verifikasiEmail.php?token='.$this->token.'"><button style="background-image: linear-gradient(to left, #0025BC , #0071BC); width: 100%; text-align: center; margin: auto; min-height: 40px; color: white; font-size: 30px; cursor: pointer;">Klik disini</button></a>';
+                    $message .='</div>';
+                    $message .='<div class="mail-footer" style="color: black; background-color: #adadad; width: 100%; font-size: 20px;padding: 20px; border-bottom-left-radius: 25px; border-bottom-right-radius: 25px;">';
+                    $message .='</div>';
+                $message .='</div>';
+            $message .='</body>';
+            $message .='</html>';
+            
+            if(mail($this->email,$email_subject,$message,$headers)){
+                return true; 
+            }else{
+                return false;
+            }
+    }
 
     function readOne(){
  
