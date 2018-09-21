510b7832f90b93c848cf083f79b85173e547eedf
frentzenzero <frentzen.zero@gmail.com>
2018-09-21 20:38:01 +0700

    cekView11

---
+++ book/readByUser.php
@@ -0,0 +1,52 @@
+<?php
+
+ 
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/book.php';
+ 
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+ 
+// prepare user object
+$book = new Book($db);
+ 
+// set ID property of user to be edited
+$catatan->id= isset($_GET['pemesan']) ? $_GET['pemesan'] : die();
+// read the details of user to be edited
+$stmtBook = $book->readOne();
+$numBook = $stmtBook->rowCount();
+
+// create array
+if($numCatatan>0){
+ 
+    // products array
+    $book_arr=array();
+    $book_arr["book"]=array();
+
+    while ($rowBook = $stmtBook->fetch(PDO::FETCH_ASSOC)){
+
+        extract($rowBook);
+        $product_item=array(
+            "id" => $id,
+            "tipeMobil" => $tipeMobil,
+            "modelMobil" => $modelMobil,
+            "transmisi" => $bookDate,
+            "bookDate" => $bookDate,
+            "returnDate" => $returnDate,
+            "harga" => $harga,
+            "pemesan" => $pemesan,
+         );
+         
+        array_push($book_arr["book"], $product_item);
+    }
+}
+else{
+    $book_arr["book"]=null;
+}
+
+ 
+// make it json format
+print_r(json_encode($user_arr));
+?>
